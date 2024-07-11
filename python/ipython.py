5a6415970880955c501cf3e698a226bf74260d9f
zachvalenta <zjv5002@gmail.com>
2024-07-11 10:37:38 -0400

    ipython: catpuccin

--- python/ipython.py
+++ python/ipython.py
@@ -1,3 +1,11 @@
 c = get_config()
+
+# ENTER/EXIT
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.confirm_exit = False
+
+# COLOR
+c.TerminalInteractiveShell.true_color = True
+c.TerminalInteractiveShell.highlighting_style = "catppuccin-mocha"
+
+
