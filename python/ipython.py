3df5876fa650498ad752396a9397594a06848002
zachvalenta <zjv5002@gmail.com>
2024-07-15 17:30:07 -0400

    shell: source .zprofile in .zshenv to workaround Zellij not using login shell

--- python/ipython.py
+++ python/ipython.py
@@ -8,4 +8,5 @@ c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.true_color = True
 c.TerminalInteractiveShell.highlighting_style = "catppuccin-mocha"
 
-
+# ZA
+c.TerminalInteractiveShell.auto_match = True
