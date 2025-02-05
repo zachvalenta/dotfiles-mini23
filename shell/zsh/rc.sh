1ea8ce469df7b44e30615f82b92cba80ef63548d
zachvalenta <zjv5002@gmail.com>
2025-02-05 11:46:51 -0500

    zsh: nvm

--- shell/zsh/rc.sh
+++ shell/zsh/rc.sh
@@ -6,3 +6,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 source /Users/zach/.config/broot/launcher/bash/br
 # eval "$(nodenv init -)"
 eval "$(zoxide init zsh)"
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
