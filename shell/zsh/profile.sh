fe233ff32eb5a3fc39f4dc2ee21e91c939c9e7bd
zachvalenta <zjv5002@gmail.com>
2026-01-08 16:19:41 -0500

    zp: t handles multiple dirs as args

--- shell/zsh/profile.sh
+++ shell/zsh/profile.sh
@@ -302,22 +302,25 @@ function ti(){
 
 function t() {
     local DIRS_ONLY=""
+    local LEVEL=""
+
+    # Check for -d flag
     if [[ "$1" == "-d" ]]; then
         DIRS_ONLY="-D"
         shift
     fi
 
-    if [ $# -eq 2 ]; then
-        eza -al $DIRS_ONLY "${EZA_TREE_FLAGS[@]}" --level="$1" "$2"
-    elif [ $# -eq 1 ]; then
-        if [[ "$1" =~ ^-?[0-9]+[.,]?[0-9]*$ ]]; then
-            eza -al $DIRS_ONLY "${EZA_TREE_FLAGS[@]}" --level="$1"
-        else
-            eza -al $DIRS_ONLY "${EZA_TREE_FLAGS[@]}" "$1"
-        fi
+    # Check if first remaining arg is a number (level)
+    if [ $# -ge 1 ] && [[ "$1" =~ ^-?[0-9]+[.,]?[0-9]*$ ]]; then
+        LEVEL="--level=$1"
+        shift
+    fi
+
+    # All remaining args are directories (or none for current dir)
+    if [ $# -eq 0 ]; then
+        eza -al $DIRS_ONLY "${EZA_TREE_FLAGS[@]}" $LEVEL
     else
-        eza -al $DIRS_ONLY "${EZA_TREE_FLAGS[@]}"
-        
+        eza -al $DIRS_ONLY "${EZA_TREE_FLAGS[@]}" $LEVEL "$@"
     fi
 }
 
