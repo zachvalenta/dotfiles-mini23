2d33c2b8d09af699522cac228299859f5d7317da
zachvalenta <zjv5002@gmail.com>
2026-03-11 07:56:10 -0400

    neovim: bootstrap lazy

--- editors/neovim/init.lua
+++ editors/neovim/init.lua
@@ -1,3 +1,18 @@
+-- LAZY BOOTSTRAP
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        'git', 'clone', '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- PLUGINS
+require('lazy').setup('plugins')
+
 -- USER CONFIG
 require 'user.option'
 require 'user.keymap'
