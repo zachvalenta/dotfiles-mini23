43046a329985f57b3f383c28ac8808481d946167
zachvalenta <zjv5002@gmail.com>
2026-03-11 21:37:21 -0400

    neovim: aerial - nested headers

---
+++ editors/neovim/plugins/aerial.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        'stevearc/aerial.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        opts = {
+            backends = { 'treesitter' },
+            open_automatic = true,
+            show_guides = true,
+            layout = {
+                default_direction = 'prefer_left',
+                width = 0.20,
+            },
+            -- keymaps for the aerial panel itself
+            keymaps = {
+                ['<Tab>'] = 'actions.jump',  -- goto header, return focus to main buffer
+            },
+            on_attach = function(bufnr)
+                -- scroll prev/next header from main buffer
+                vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+                vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+                -- focus aerial panel
+                vim.keymap.set('n', '<Tab>', '<cmd>AerialToggle!<CR>', { buffer = bufnr })
+            end,
+        },
+    },
+}
