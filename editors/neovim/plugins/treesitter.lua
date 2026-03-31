da0ee8347a4ece003326a601cdcfbc7f2c75985d
zachvalenta <zjv5002@gmail.com>
2026-03-30 20:40:25 -0400

    neovim: code block syntax highlighting

--- editors/neovim/plugins/treesitter.lua
+++ editors/neovim/plugins/treesitter.lua
@@ -2,6 +2,22 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        opts = {},
+        config = function()
+            require('nvim-treesitter.config').setup({
+                ensure_installed = {
+                    'lua', 'python', 'bash', 'markdown', 'markdown_inline',
+                    'sql', 'javascript', 'html', 'css',
+                    'yaml', 'json', 'toml', 'dockerfile', 'go',
+                },
+            })
+            -- enable treesitter as the active highlighter for markdown
+            -- (required for fenced code block injection to work)
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'markdown',
+                callback = function(args)
+                    vim.treesitter.start(args.buf)
+                end,
+            })
+        end,
     },
 }
