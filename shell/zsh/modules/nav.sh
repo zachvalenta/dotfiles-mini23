5f9cdd0c5a56e1494cc3926caff899b35da928b6
zachvalenta <zjv5002@gmail.com>
2026-08-12 12:57:07 -0400

    vsc: snippet for nota bene

--- shell/zsh/modules/nav.sh
+++ shell/zsh/modules/nav.sh
@@ -41,6 +41,7 @@ alias ligo="cd_quiet /Users/zach/Documents/zv/work/colligo; t 2"
 alias cl="cd_quiet /Users/zach/Documents/zv/work/colligo/docs/worklogs; t 2"
 alias mob="cd_quiet /Users/zach/Documents/zv/work/colligo/src/mobile; eza -l"
 alias ce="cd_quiet /Users/zach/Documents/zv/work/colligo/docs/eng; t 2"
+alias org="cd_quiet /Users/zach/Documents/zv/work/colligo/docs/org; t 2"
 
 ###
 # PERSONAL
